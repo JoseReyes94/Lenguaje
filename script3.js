500711e96a4860c819702c637c71cc8c715d6aa3
JoseReyes94 <129469542+JoseReyes94@users.noreply.github.com>
2023-06-05 22:04:16 -0500

    Update script3.js

--- script3.js
+++ script3.js
@@ -43,4 +43,61 @@ function inprimirEdad(){
     console.log(`${nombre} tiene ${edad} años `)
 }
 
-inprimirEdad()
+inprimirEdad();
+inprimirEdad(nombre,edad);
+inprimirEdad("Jose", 28);
+inprimirEdad("Ana", 21);
+
+function sumar (a,b){
+    var total = a + b;
+    return total;
+}
+
+var totalSuma = sumar(14,15);
+console.log(totalSuma);
+
+var nombreJosé = 'José'
+function inprimirnombreEnMayusculas(nombre){
+    nombre = nombre.toUpperCase();
+    console.log(nombre);
+
+}
+
+inprimirnombreEnMayusculas(nombreJosé)
+
+
+var persona2 = {
+    nombre: 'José',
+    apellido: 'Reyes',
+    edad: 29
+}
+function imprimirnombreEnMayusculas(obj){
+var name = obj.nombre.toUpperCase();
+    console.log(name);
+}
+
+imprimirnombreEnMayusculas(persona2)
+
+var persona2 = {
+    nombre: 'José',
+    apellido: 'Reyes',
+    edad: 29
+}
+    
+function imprimirnombreEnMayusculas(obj) {
+        //var nombre = objeto nombre
+        var {nombre,apellido} = obj;
+        console.log(nombre + " " + apellido);
+        } 
+
+        var persona2 = {
+            nombre: 'José',
+            edad: 29
+        }
+
+function inprimirnombreyEdad(obj) {
+    var {nombre,edad} = obj;
+    console.log("Hola, me llamo " +  nombre + " tengo " + edad + " años");
+        } 
+
+        inprimirnombreyEdad(persona2)
